9b8f2d41682199f3449fe21d4d939aa3ea372030
Danny Wachsstock <d.wachss@prodigy.net>
2020-05-24 16:57:25 -0500

    refactor

--- KavanotParsedown.php
+++ KavanotParsedown.php
@@ -3,22 +3,38 @@ require_once ('Parsedown.php');
 
 class KavanotParsedown extends Parsedown {
 
-	function __construct(){
-		$this->InlineTypes['/'] []= 'Italic';
-		$this->InlineTypes['{'] []= 'Attributes';
-		$this->InlineTypes['_'] = ['Cite']; // redefinition
-		$this->inlineMarkerList = implode ('', array_keys($this->InlineTypes));
-		$this->specialCharacters []= '/'; // only characters in this array can be escaped by '\'
-		$this->BlockTypes['-'][] = 'Source';
-	}
-	
-	protected $smartQuotes = array(
+	protected $reItalic = '#^\/(.+?)\/#';
+	protected $reCite = '#^_(.+?)_#';
+	protected $reAttributes = '#^{:(.+?)}\s*#';
+	protected $reSource = '/^--[ ]*(.+)/';
+	protected $reSmartQuotes = array(
 		'"..."' => 'adjustEllipsis',
 		'"--"' => 'adjustEmDash',
 		"'\"'" => 'adjustDoubleQuotes',
 		'"\'"' => 'adjustSingleQuotes'
 	);
 		
+
+	function __construct(){
+		$this->InlineTypes['/'] []= 'Italic';
+		$this->InlineTypes['_'] = ['Cite']; // redefinition
+		$this->InlineTypes['{'] []= 'Attributes';
+		$this->inlineMarkerList = implode ('', array_keys($this->InlineTypes));
+		$this->specialCharacters []= '/'; // only characters in this array can be escaped by '\'
+		$this->BlockTypes['-'][] = 'Source';
+		
+		// String utility functions
+		$strings = &$this->strings;
+		$this->remover = function ($matches) use ($strings) {
+			$strings []= $matches[0];
+			return "\x1a".count($strings)."\x1a";
+		};
+		$this->replacer = function ($matches) use ($strings) {
+			return $this->strings[$matches[1]-1];
+		};
+
+	}
+	
 	function text($text){
 		$text = parent::text($text);
 		
@@ -39,7 +55,7 @@ class KavanotParsedown extends Parsedown {
 		foreach ($xpath->query("//attr") as $attr) self::moveAttributes ($attr);
 				
 		// smart quotes etc.
-		foreach ($this->smartQuotes as $plain => $fancy){
+		foreach ($this->reSmartQuotes as $plain => $fancy){
 			foreach ($xpath->query("//text()[contains(.,$plain)]") as $node){ // select only text nodes with the target string
 				// note that this will fail if the quotes span multiple text nodes. Need to manually edit for that.
 				$this->$fancy ($node);
@@ -49,10 +65,167 @@ class KavanotParsedown extends Parsedown {
 		return $dom->saveHTML($dom->documentElement);
 	} // text
 	
-	static protected function copyAttributes ($from, $to){
-		foreach ($from->attributes as $attr) $to->setAttribute($attr->nodeName, $attr->nodeValue);
+//----- Italic ------
+	// foreign languages use the <i> tag. Default for me is Hebrew
+	protected function inlineItalic($excerpt){
+		if (preg_match($this->reItalic, $excerpt['text'], $matches)) {
+			return array(
+				'extent' => strlen($matches[0]), 
+				'element' => array(
+					'name' => 'i',
+					'handler' => array(
+						'function' => 'lineElements',
+						'argument' => $matches[1],
+						'destination' => 'elements'
+					),
+					'attributes' => array('lang' => 'he')
+				)
+			);
+		}
+	}	// inlineItalic
+	
+//----- Cite ----
+	protected function inlineCite($excerpt){
+		if (preg_match($this->reCite, $excerpt['text'], $matches)) {
+			return array(
+				'extent' => strlen($matches[0]), 
+				'element' => array(
+					'name' => 'cite',
+					'handler' => array(
+						'function' => 'lineElements',
+						'argument' => $matches[1],
+						'destination' => 'elements'
+					)
+				)
+			);
+		}
+	}	// inlineCite
+
+//---- Attributes -----
+	protected function inlineAttributes($excerpt){
+		// based on https://python-markdown.github.io/extensions/attr_list/
+		// but the {: attr list } goes before the element, not after
+		if (preg_match($this->reAttributes, $excerpt['text'], $matches)) {
+			return array(
+				'extent' => strlen($matches[0]), 
+				'element' => array(
+					'name' => 'attr',
+					'attributes' => $this->parseAttributes($matches[1])
+				)
+			);
+		}
+	}	// inlineAttributes
+	
+	// allow for attribute lists before blocks; they should be in their own paragraph
+	protected function paragraphContinue($Line, array $Block){
+		if (!$Block['interrupted'] && preg_match ($this->reAttributes,  $Block['element']['handler']['argument'])){
+			$Block['interrupted'] = 1;
+		}
+		return parent::paragraphContinue($Line, $Block);
 	}
 	
+//---- Source -----
+	protected function blockSource($Line, $Block = null){
+		if ($Block && $Block['type'] === 'Paragraph'){
+			// parse this immediately and add it to the paragraph (since paragraphs don't nest block-level elements)
+			$Block['element']['handler']['argument'] .= "\n".$this->element($this->blockSource($Line)['element']);
+			return $Block;
+		}
+		if (preg_match('/^--[ ]*(.+)/', $Line['text'], $matches)) {
+			return array(
+				'element' => array(
+					'name' => 'footer',
+					'handler' => array(
+						'function' => 'lineElements',
+						'argument' => $matches[1],
+						'destination' => 'elements'
+					),
+					'attributes' => array('class' => 'source')
+				)
+			);
+		}
+	}	// blockSource
+
+	protected function blockSourceContinue($Line, array $Block){
+		if (!isset($Block['interrupted']) && preg_match('/^--[ ]*(.+)/', $Line['text'], $matches)){
+			$Block['element']['handler']['argument'] .= $matches[1];
+			return $Block;
+		}
+	}
+
+//---- Smart Quotes -----
+	protected function adjustEllipsis ($node){
+		$node->textContent = preg_replace ('/\.\.\./', '…', $node->textContent);
+	}
+
+	protected function adjustEmDash ($node){
+		$node->textContent = preg_replace ('/--/', '—', $node->textContent);
+	}
+	
+	protected function adjustDoubleQuotes ($node){
+		$gershayim = '״'; // these are very hard to see in my editor, so I separated these out
+		$hebrew = '[א-ת]';
+		$node->textContent = preg_replace ("/($hebrew)\"($hebrew)/", "$1$gershayim$2", $node->textContent);
+		$node->textContent = preg_replace ("/($hebrew)(\W*)\"(.+?)\"/", '$1$2”$3“', $node->textContent);  // if preceded by Hebrew, assume it's right to left
+		$node->textContent = preg_replace ('/"(.+?)"/', '“$1”', $node->textContent);
+	}
+
+	protected function adjustSingleQuotes ($node){
+		$geresh = '׳'; // these are very hard to see in my editor, so I separated these out
+		$hebrew = '[א-ת]';
+		$node->textContent = preg_replace ("/($hebrew)'(\\W)/", "$1$geresh$2", $node->textContent);
+		$node->textContent = preg_replace ("/($hebrew)(\\W*)'(.+?)'/", '$1$2’$3‘', $node->textContent);  // if preceded by Hebrew, assume it's right to left
+		$node->textContent = preg_replace ('/(\w)\'(\w)/', '$1’$2', $node->textContent); // apostrophe
+		$node->textContent = preg_replace ('/\'(.+?)\'/', '‘$1’', $node->textContent); // single quotes
+	}
+
+//---- Utility functions for Attributes ----
+	protected function parseAttributes ($attrString){
+		// shortcuts:
+		// #string is translated to id="string"
+		// .string is translated to class="string"
+		// [a-zA-Z]{2} is translated to lang=[a-zA-Z]{2} (since I use the lang= attribute so much)
+		$attrString = " $attrString "; // to simplify the regex, search for space-delimited rather than start/end
+		// to make this work, we need to actually parse the string; simply splitting on spaces won't account for strings
+		// so we pull out quotes. Fortunately HTML doesn't escape quotes; you need to use &quot;
+		$attrstring = $this->removeStrings ('/"[^"]*"/', $attrString);
+		$attrstring = $this->removeStrings ("/'[^']*'/", $attrString);
+		$attrString = preg_replace ('/ #(\w+)(?= )/', ' id=$1 ', $attrString);
+		$attrString = preg_replace ('/ \.(\w+)(?= )/', ' class=$1 ', $attrString);
+		$attrString = preg_replace ('/ ([a-zA-Z]{2})(?= )/', ' lang=$1 ', $attrString);
+		$attrString = $this->replaceStrings($attrString);
+		// trick from https://stackoverflow.com/a/1083843, though SimpleXMLElement is much more strict so I had to use DOMDocument
+		$dom = DOMDocument::loadHTML("<element $attrString />",
+			LIBXML_HTML_NOIMPLIED | LIBXML_NOWARNING | LIBXML_NOERROR | LIBXML_HTML_NODEFDTD);
+		$ret = [];
+		foreach ($dom->firstChild->attributes as $attr){
+			$ret [$attr->nodeName] = $attr->nodeValue;
+		}
+		return $ret;
+	} // parseAttributes
+	
+	static protected function moveAttributes ($attrNode){
+		// apply the attributes of $attrNode to the next node, then delete it.
+		// Special case: an attribute on a line by itself will be enclosed in a <p>. Delete that <p>
+		if ($attrNode->parentNode->childNodes->length == 1 && $attrNode->parentNode->nodeName == 'p'){
+			self::copyAttributes ($attrNode->parentNode, $attrNode);
+			$attrNode->parentNode->parentNode->replaceChild ($attrNode, $attrNode->parentNode);
+		}
+		// skip text nodes etc.
+		for ($target = $attrNode->nextSibling; $target && $target->nodeType != XML_ELEMENT_NODE; $target = $target->nextSibling);
+		if ($target) self::copyAttributes ($attrNode, $target);
+		$attrNode->parentNode->removeChild($attrNode);
+	} // moveAttributes
+
+	static protected function copyAttributes ($from, $to){
+		try{
+			foreach ($from->attributes as $attr) $to->setAttribute($attr->nodeName, $attr->nodeValue);
+		}catch(Exception $e){
+			// ignore errors; generally are illegal characters in the attribute name
+		}
+	}
+
+//---- utility functions for Source	
 	static protected function adjustFooter ($footer){
 		// In my original Kavanot, sources are in the form
 		// <blockquote lang=he>
@@ -83,164 +256,19 @@ class KavanotParsedown extends Parsedown {
 		$figure->appendChild ($figcaption);
 		$footer->parentNode->removeChild ($footer);
 	}
-
-	static protected function parseAttributes ($attrString){
-		// shortcuts:
-		// #string is translated to id="string"
-		// .string is translated to class="string"
-		// [a-zA-Z]{2} is translated to lang=[a-zA-Z]{2} (since I use the lang= attribute so much)
-		$attrString = " $attrString "; // to simplify the regex, search for space-delimited rather than start/end
-		// to make this work, we need to actually parse the string; simply splitting on spaces won't account for strings
-		// so we pull out quotes. Fortunately HTML doesn't escape quotes; you need to use &quot;
-		$strings = array();
-		$remover = function ($matches) use (&$strings) {
-			$strings []= $matches[0];
-			return "\x1a".count($strings)."\x1a";
-		};
-		$replacer = function ($matches) use (&$strings) {
-			return $strings[$matches[1]-1];
-		};
-		$attrString = preg_replace_callback ('/"[^"]*"/', $remover, $attrString);
-		$attrString = preg_replace_callback ("/'[^']*'/", $remover, $attrString);
-		$attrString = preg_replace ('/ #(\w+) /', ' id=$1 ', $attrString);
-		$attrString = preg_replace ('/ \.(\w+) /', ' class=$1 ', $attrString);
-		$attrString = preg_replace ('/ ([a-zA-Z]{2}) /', ' lang=$1 ', $attrString);
-		$attrString = preg_replace_callback ('/\x1a(\d)+\x1a/', $replacer, $attrString);
-		// trick from https://stackoverflow.com/a/1083843, though SimpleXMLElement is much more strict so I had to use DOMDocument
-		$dom = DOMDocument::loadHTML("<element $attrString />",
-			LIBXML_HTML_NOIMPLIED | LIBXML_NOWARNING | LIBXML_NOERROR | LIBXML_HTML_NODEFDTD);
-		$ret = [];
-		foreach ($dom->firstChild->attributes as $attr){
-			$ret [$attr->nodeName] = $attr->nodeValue;
-		}
-		return $ret;
-	} // parseAttributes
 	
-	static protected function moveAttributes ($attrNode){
-		// apply the attributes of $attrNode to the next node, then delete it.
-		// Special case: an attribute on a line by itself will be enclosed in a <p>. Delete that <p>
-		if ($attrNode->parentNode->childNodes->length == 1 && $attrNode->parentNode->nodeName == 'p'){
-			self::copyAttributes ($attrNode->parentNode, $attrNode);
-			$attrNode->parentNode->parentNode->replaceChild ($attrNode, $attrNode->parentNode);
-		}
-		// skip text nodes etc.
-		for ($target = $attrNode->nextSibling; $target && $target->nodeType != XML_ELEMENT_NODE; $target = $target->nextSibling);
-		if ($target) self::copyAttributes ($attrNode, $target);
-		$attrNode->parentNode->removeChild($attrNode);
-	} // moveAttributes
-	
-	protected function adjustEllipsis ($node){
-		$node->textContent = preg_replace ('/\.\.\./', '…', $node->textContent);
-	}
+//---- String utilities
 
-	protected function adjustEmDash ($node){
-		$node->textContent = preg_replace ('/--/', '—', $node->textContent);
+	private $strings = array();
+	private $remover; // these functions need to be defined in the constructor
+	private $replacer;
+
+	protected function removeStrings ($re, $target){
+		return preg_replace_callback ($re, $this->remover, $target);
 	}
 	
-	protected function adjustDoubleQuotes ($node){
-		$gershayim = '״'; // these are very hard to see in my editor, so I separated these out
-		$hebrew = '[א-ת]';
-		$node->textContent = preg_replace ("/($hebrew)\"($hebrew)/", "$1$gershayim$2", $node->textContent);
-		$node->textContent = preg_replace ("/($hebrew)(\W*)\"(.+?)\"/", '$1$2”$3“', $node->textContent);  // if preceded by Hebrew, assume it's right to left
-		$node->textContent = preg_replace ('/"(.+?)"/', '“$1”', $node->textContent);
-	}
-
-	protected function adjustSingleQuotes ($node){
-		$geresh = '׳'; // these are very hard to see in my editor, so I separated these out
-		$hebrew = '[א-ת]';
-		$node->textContent = preg_replace ("/($hebrew)'(\W)/", "$1$geresh$2", $node->textContent);
-		$node->textContent = preg_replace ("/($hebrew)(\W*)'(.+?)'/", '$1$2’$3‘', $node->textContent);  // if preceded by Hebrew, assume it's right to left
-		$node->textContent = preg_replace ('/(\w)\'(\w)/', '$1’$2', $node->textContent); // apostrophe
-		$node->textContent = preg_replace ('/\'(.+?)\'/', '‘$1’', $node->textContent); // single quotes
-	}
-
-	// foreign languages use the <i> tag. Default for me is Hebrew
-	protected function inlineItalic($excerpt){
-		if (preg_match('#^\/(.+?)\/#', $excerpt['text'], $matches)) {
-			return array(
-				// How many characters to advance the Parsedown's
-				// cursor after being done processing this tag.
-				'extent' => strlen($matches[0]), 
-				'element' => array(
-					'name' => 'i',
-					'handler' => array(
-						'function' => 'lineElements',
-						'argument' => $matches[1],
-						'destination' => 'elements'
-					),
-					'attributes' => array('lang' => 'he')
-				)
-			);
-		}
-	}	// inlineItalic
-	
-	protected function inlineCite($excerpt){
-		if (preg_match('#^_(.+?)_#', $excerpt['text'], $matches)) {
-			return array(
-				'extent' => strlen($matches[0]), 
-				'element' => array(
-					'name' => 'cite',
-					'handler' => array(
-						'function' => 'lineElements',
-						'argument' => $matches[1],
-						'destination' => 'elements'
-					)
-				)
-			);
-		}
-	}	// inlineCite
-	
-	private $attributeRE = '#^{:(.+?)}\s*#';
-
-	protected function inlineAttributes($excerpt){
-		// based on https://python-markdown.github.io/extensions/attr_list/
-		// but the {: attr list } goes before the element, not after
-		if (preg_match($this->attributeRE, $excerpt['text'], $matches)) {
-			return array(
-				'extent' => strlen($matches[0]), 
-				'element' => array(
-					'name' => 'attr',
-					'attributes' => self::parseAttributes($matches[1])
-				)
-			);
-		}
-	}	// inlineCite
-	
-	// allow for attribute lists before blocks; they should be in their own paragraph
-	protected function paragraphContinue($Line, array $Block){
-		if (!$Block['interrupted'] && preg_match ($this->attributeRE,  $Block['element']['handler']['argument'])){
-			$Block['interrupted'] = 1;
-		}
-		return parent::paragraphContinue($Line, $Block);
-	}
-
-	// Sources use a footer in a block quote. Use -- to indicate this)
-	protected function blockSource($Line, $Block = null){
-		if ($Block && $Block['type'] === 'Paragraph'){
-			// parse this immediately and add it to the paragraph (since paragraphs don't nest block-level elements)
-			$Block['element']['handler']['argument'] .= "\n".$this->element($this->blockSource($Line)['element']);
-			return $Block;
-		}
-		if (preg_match('/^--[ ]*(.+)/', $Line['text'], $matches)) {
-			return array(
-				'element' => array(
-					'name' => 'footer',
-					'handler' => array(
-						'function' => 'lineElements',
-						'argument' => $matches[1],
-						'destination' => 'elements'
-					),
-					'attributes' => array('class' => 'source')
-				)
-			);
-		}
-	}	// blockSource
-
-	protected function blockSourceContinue($Line, array $Block){
-		if (!isset($Block['interrupted']) && preg_match('/^--[ ]*(.+)/', $Line['text'], $matches)){
-			$Block['element']['handler']['argument'] .= $matches[1];
-			return $Block;
-		}
+	protected function replaceStrings ($target){
+		return preg_replace_callback ('/\x1a(\d)+\x1a/', $this->replacer, $target);
 	}
 	
 } // KavanotParsedown
