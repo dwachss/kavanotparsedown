5f5641be281719ba497a7a0625cfb5e4b5838b62
Danny Wachsstock <d.wachss@prodigy.net>
2020-05-25 17:36:37 -0500

    refactor string replacement

--- KavanotParsedown.php
+++ KavanotParsedown.php
@@ -1,6 +1,29 @@
 <?
 require_once ('Parsedown.php');
 
+// for a lot of the string manipulation that I do, I need to remove a section of the text, process the remainder, 
+// and then put it back. I want to make sure that the placeholder is not something that might occur in the original text,
+// so it is not replaced accidentally. It has to be legal XML, though. U+FFFC (https://en.wikipedia.org/wiki/Specials_(Unicode_block) ) 
+// works for that. Removed strings are marked with  "￼{number}￼".
+define ('OBJECT_REPLACEMENT_CHARACTER', '￼');
+class StringReplace {
+	static private $strings = array();
+	static private $reReplacement = '/'.OBJECT_REPLACEMENT_CHARACTER.'(\d)+'.OBJECT_REPLACEMENT_CHARACTER.'/';
+	private static function remover ($matches){
+		self::$strings []= $matches[0];
+		return OBJECT_REPLACEMENT_CHARACTER.count(self::$strings).OBJECT_REPLACEMENT_CHARACTER;
+	}
+	private static function replacer ($matches){
+		return self::$strings[$matches[1]-1];
+	}
+	static public function remove ($re, $target){
+		return preg_replace_callback ($re, 'self::remover', $target);
+	}
+	static public function restore ($target){
+		return preg_replace_callback (self::$reReplacement, 'self::replacer', $target);
+	}
+}
+
 class KavanotParsedown extends Parsedown {
 
 	protected $reItalic = '#^\/(.+?)\/#';
@@ -22,17 +45,6 @@ class KavanotParsedown extends Parsedown {
 		$this->inlineMarkerList = implode ('', array_keys($this->InlineTypes));
 		$this->specialCharacters []= '/'; // only characters in this array can be escaped by '\'
 		$this->BlockTypes['-'][] = 'Source';
-		
-		// String utility functions
-		$strings = &$this->strings;
-		$this->remover = function ($matches) use (&$strings) {
-			$strings []= $matches[0];
-			return "\x1a".count($strings)."\x1a";
-		};
-		$this->replacer = function ($matches) use (&$strings) {
-			return $strings[$matches[1]-1];
-		};
-
 	}
 	
 	function text($text){
@@ -43,7 +55,6 @@ class KavanotParsedown extends Parsedown {
 		// https://stackoverflow.com/a/16931835 to use UTF-8
 		// https://stackoverflow.com/a/22490902 to not add DTD
 		// https://stackoverflow.com/a/29499398 to wrap it in an <div> element (since it needs a single root)
-		// we could strip that off, but I'm not concerned about the nonsemantic html
 		$dom = DOMDocument::loadHTML('<?xml encoding="UTF-8"><div>'.$text.'</div>',
 			LIBXML_HTML_NOIMPLIED | LIBXML_NOWARNING | LIBXML_NOERROR | LIBXML_HTML_NODEFDTD);
 		$xpath = new DOMXpath($dom);
@@ -53,6 +64,9 @@ class KavanotParsedown extends Parsedown {
 		
 		// turn <attr> elements into attributes of the following element
 		foreach ($xpath->query("//attr") as $attr) self::moveAttributes ($attr);
+		
+		// process elements with the markdown attribute
+		foreach ($xpath->query("//*[@markdown] | //*[@md]") as $element) $this->processElement ($element);
 				
 		$text = $dom->saveHTML($dom->documentElement);
 
@@ -61,7 +75,12 @@ class KavanotParsedown extends Parsedown {
 		foreach ($this->reSmartQuotes as $plain => $fancy){
 			$text = $this->$fancy ($text);
 		}
-		$text = $this->replaceStrings($text);
+
+		// undo all the string removal
+		$text = StringReplace::restore ($text);
+		
+		// remove the nonsemantic <div>
+		$text = preg_replace ('#^<div>|</div>$#', '', $text);
 
 		return $text;
 	} // text
@@ -154,6 +173,17 @@ class KavanotParsedown extends Parsedown {
 		}
 	}
 
+//---- Markdown inside HTML blocks
+	protected function processElement($e){
+		$outerHTML = $e->ownerDocument->saveHTML($e);
+		$innerHTML = preg_replace ('#(^[^>]*>)|(<[^<]*$)#', '', $outerHTML);
+		$innerHTML = $this->text ($innerHTML);
+		$replacementString = StringReplace::remove ('/^.*$/s', $innerHTML); // remove the entire innerHTML
+		$e->removeAttribute ('markdown');
+		$e->removeAttribute ('md');
+		$e->textContent = $replacementString;
+	}
+
 //---- Smart Quotes -----
 	protected function adjustEllipsis ($text){
 		return $this->preg_replace_text ('/\.\.\./', '…', $text);
@@ -191,12 +221,12 @@ class KavanotParsedown extends Parsedown {
 		$attrString = " $attrString "; // to simplify the regex, search for space-delimited rather than start/end
 		// to make this work, we need to actually parse the string; simply splitting on spaces won't account for strings
 		// so we pull out quotes. Fortunately HTML doesn't escape quotes; you need to use &quot;
-		$attrstring = $this->removeStrings ('/"[^"]*"/', $attrString);
-		$attrstring = $this->removeStrings ("/'[^']*'/", $attrString);
+		$attrString = StringReplace::remove ('/"[^"]*"/', $attrString);
+		$attrString = StringReplace::remove ("/'[^']*'/", $attrString);
 		$attrString = preg_replace ('/ #(\w+)(?= )/', ' id=$1 ', $attrString);
 		$attrString = preg_replace ('/ \.(\w+)(?= )/', ' class=$1 ', $attrString);
 		$attrString = preg_replace ('/ ([a-zA-Z]{2})(?= )/', ' lang=$1 ', $attrString);
-		$attrString = $this->replaceStrings($attrString);
+		$attrString = StringReplace::restore ($attrString);
 		// trick from https://stackoverflow.com/a/1083843, though SimpleXMLElement is much more strict so I had to use DOMDocument
 		$dom = DOMDocument::loadHTML("<element $attrString />",
 			LIBXML_HTML_NOIMPLIED | LIBXML_NOWARNING | LIBXML_NOERROR | LIBXML_HTML_NODEFDTD);
@@ -261,22 +291,9 @@ class KavanotParsedown extends Parsedown {
 	}
 	
 //---- String utilities
-
-	private $strings = array();
-	private $remover; // these functions need to be defined in the constructor
-	private $replacer;
-
-	protected function removeStrings ($re, $target){
-		return preg_replace_callback ($re, $this->remover, $target);
-	}
-	
-	protected function replaceStrings ($target){
-		return preg_replace_callback ('/\x1a(\d)+\x1a/', $this->replacer, $target);
-	}
-
 	protected function removeInlineTags ($text){
 		$reInlineTag = '#</?('.implode('|',$this->textLevelElements).')[^>]*>#';
-		return $this->removeStrings ($reInlineTag, $text);
+		return StringReplace::remove ($reInlineTag, $text);
 	}
 
 	protected function preg_replace_text ($pattern, $replacement, $subject){
